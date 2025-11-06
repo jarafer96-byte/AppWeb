9b3ff33347b9c16f8390df078acf9df7c1f1a17b
jarafer96-byte <jarafer96@gmail.com>
2025-11-06 18:46:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ import shortuuid  # ← ya la tenés instalada, ¿no?
 import mercadopago
 from flask import jsonify
 from datetime import datetime
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-if not firebase_admin._apps:
-    cred = credentials.ApplicationDefault()  # o Certificate("ruta/clave.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()  # ✅ Ahora sí funciona
-
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4 MB
