cd57ce0992b90e36b6f7cdd3e363c0f7d2e53f85
Fernando Jara <jarafer96@gmail.com>
2025-11-12 21:57:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ def step3():
                     with open(ruta_tmp, "rb") as f:
                         contenido_bytes = f.read()
                     resultado_github = subir_archivo(
-                        repo_name,
+                        nombre_repo,
                         contenido_bytes,
                         f"static/img/{imagen_base}",
                         token   # ✅ ahora siempre la misma variable
