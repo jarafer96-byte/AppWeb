36236a6237aef4c79165363c6dc4ecac7b111510
Fernando Jara <jarafer96@gmail.com>
2026-03-13 00:35:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ from urllib.parse import urlencode, quote, unquote
 from google.cloud.firestore import ArrayUnion
 from flask_talisman import Talisman
 from flask_cors import CORS
+from google.auth.transport.requests import Request
 import builtins
 builtins.print = lambda *args, **kwargs: None
 ##################
